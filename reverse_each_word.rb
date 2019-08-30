a15f45ad6d3c85c3b60f01b803ef2ef00ef053ff
Eddie Parker <eddie.parker88@gmail.com>
2019-08-30 21:14:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,10 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   new_sentence = []
   words.each do |flip|
-    
+    end_loop = word.length
+		new_string = ''
+		while end_loop > 0
+			end_loop -= 1
+			new_string += word[end_loop] 
   
 end
